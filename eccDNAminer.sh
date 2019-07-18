fad01795d32c1a37f90eca1d27987cbe0323c424
Zefeng Wu <835102330@qq.com>
2019-07-18 16:43:00 +0800

    Update eccDNAminer.sh

--- eccDNAminer.sh
+++ eccDNAminer.sh
@@ -9,3 +9,10 @@ NanoPlot -t 40 --fastq  all.fastq --plots hex dot
 ## fastq convert fasta
 sed -n '1~4s/^@/>/p;2~4p' fastq_runid_d9cccf88836673dacb0304fed8096fd4eca4f0fb_0.fastq > test.fasta
 
+
+
+./trf409.linux64  /home/wuzefeng/MyResearch/genome.db/TAIR/dna/Arabidopsis_thaliana.TAIR10.31.dna.toplevel.fa 2 7 7 80 10 100 2000 -d -h
+
+./trf409.linux64 fasta文件名 match得分 错配得分 indel罚分 PM PI 最小比对得分 tandem覆盖的序列长度 -d -h(阻止html)
+
+
