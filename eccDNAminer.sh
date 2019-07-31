a4b3874dc688eb42a81585c2b3ebcf3b076aae27
Zefeng Wu <835102330@qq.com>
2019-07-31 20:35:43 +0800

    Update eccDNAminer.sh

--- eccDNAminer.sh
+++ eccDNAminer.sh
@@ -1,3 +1,4 @@
+## method one: TRF
 mkdir pass_merge
 unzip pass.zip
 for m in $(ls *.gz); do gunzip $m ; done
@@ -6,7 +7,7 @@ cat *.fastq > ../pass_merge/all.fastq
 ### reads quality statistics
 NanoPlot -t 40 --fastq  all.fastq --plots hex dot
 
-## fastq convert fasta
+## fastq convert to fasta
 sed -n '1~4s/^@/>/p;2~4p' all.fastq > all.fasta
 
 ## perform tandem repeat finder to seach TRs in reads
@@ -22,3 +23,30 @@ sed -n '1~4s/^@/>/p;2~4p' all.fastq > all.fasta
 ### format convert
 python test.py in.dat out.txt
 
+
+
+### method 2: TideHunter
+TideHunter all.fastq -t 40  -o 4TideHunter_result/cons.fa
+
+## minimap2
+minimap2 -ax map-ont ../reference_genome/Drosophila_melanogaster.BDGP6.22.dna.toplevel.fa  4TideHunter_result/cons.fa -t 30 --sam-hit-only --secondary=no -o 5minimap2_result/minimap2.sam
+
+## sam to bam
+samtools view -S -b minimap2.sam > minimap2.bam
+
+## obtain aligned reads
+samtools view -bF 4 minimap2.bam > 3minimap2_aligned.bam
+
+## sort sam
+java -jar ~/soft/picard.jar  SortSam I= 3minimap2_aligned.bam O= 4minimap2_aligned_sorted.bam SORT_ORDER=coordinate
+
+## load into R 
+library(genomicFeatures)
+library(bamsignals)
+
+tr<-makeTxDbFromGFF("../../reference_genome/Drosophila_melanogaster.BDGP6.22.97.gtf")
+genes<-genes(tr)
+
+covSigs <- bamCount("4minimap2_aligned_sorted.bam", genes)
+genes$reads_count <-covSigs
+write.table(as.data.frame(genes),"out",sep="\t",col.names=TRUE,row.names=TRUE,quot=FALSE)
